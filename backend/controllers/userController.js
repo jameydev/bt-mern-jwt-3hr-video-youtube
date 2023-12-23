0a8b13b63cf4e8edb8d9909c951d3358342ef540
jameydev <jybryce4@gmail.com>
2023-12-23 05:23:18 -0600

    routes started

---
+++ backend/controllers/userController.js
@@ -0,0 +1,10 @@
+// @desc Auth user/set token
+// @route POST /api/users/auth
+// @access Public
+const authUser = (req, res) => {
+    res.status(200).json({ message: 'User is authenticated' });
+};
+
+export {
+    authUser
+};
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import userRoutes from './routes/userRoutes.js';
+
 dotenv.config();
 
 const app = express();
 
 const port = process.env.PORT || 8000;
 
+app.use('/api/users', userRoutes);
+
 app.get('/', (req, res) => {
-  res.send('Server is ready');
+    res.send('Server is ready');
 });
 
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+    console.log(`Server running on http://localhost:${port}`);
 });
