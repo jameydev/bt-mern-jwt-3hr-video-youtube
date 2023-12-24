c2cdbe5462b5688ba3848f664dd2aeec871a8b0a
jameydev <jybryce4@gmail.com>
2023-12-24 17:17:18 -0600

    using redux - added toastify dependency

---
+++ frontend/src/slices/usersApiSlice.js
@@ -0,0 +1,18 @@
+import { apiSlice } from './apiSlice';
+
+const USERS_URL = '/api/users/';
+
+export const usersApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        login: builder.mutation({
+            query: (credentials) => ({
+                url: `${USERS_URL}/auth`,
+                method: 'POST',
+                body: credentials,
+            }),
+            invalidatesTags: ['User'],
+        }),
+    })
+});
+
+export const { useLoginMutation } = usersApiSlice;
