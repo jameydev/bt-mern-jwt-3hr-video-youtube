5fa8697f490fbe73527390f75a5d6fcf72de7300
jameydev <jybryce4@gmail.com>
2023-12-24 14:16:27 -0600

    LinkContainer used

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,18 +1,24 @@
 import { Navbar, Nav, Container } from 'react-bootstrap';
 import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap';
 
 export default function Header() {
     return (
         <header>
-
             <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">MERN Auth</Navbar.Brand>
+                    <LinkContainer to="/">
+                        <Navbar.Brand>MERN Auth</Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ms-auto">
-                            <Nav.Link href="/login"><FaSignInAlt /> Login</Nav.Link>
-                            <Nav.Link href="/register"><FaSignInAlt /> Register</Nav.Link>
+                            <LinkContainer to="/login">
+                                <Nav.Link><FaSignInAlt /> Login</Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to="/register">
+                                <Nav.Link><FaSignInAlt /> Register</Nav.Link>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
