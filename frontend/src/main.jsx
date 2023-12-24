a770cd86f0d49efb7c698c567faad4ca05a6c073
jameydev <jybryce4@gmail.com>
2023-12-24 15:19:16 -0600

    register form / screen - more frontend deps

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -11,12 +11,14 @@ import App from './App.jsx'
 import './index.css'
 import HomeScreen from './screens/HomeScreen.jsx';
 import LoginScreen from './screens/LoginScreen.jsx';
+import RegisterScreen from './screens/RegisterScreen.jsx';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path="/" element={<App />}>
             <Route index={true} path="/" element={<HomeScreen />} />
             <Route path="/login" element={<LoginScreen />} />
+            <Route path="/register" element={<RegisterScreen />} />
         </Route>
     )
 );
